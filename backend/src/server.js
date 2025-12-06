4f342a460fe93eb802b92bb9ed0d9b4ed3c5794e
BKarthik7 <bangikarthik7@gmail.com>
2025-12-07 01:12:01 +0530

    Install vite and @vitejs/plugin-react as prod deps for Render build

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import path from 'path';
+import { ENV } from './config/env.js';
 
 const app = express();
+const __dirname = path.resolve();
 
 app.get('/api/health', (req, res) => {
     res.status(200).json({ message: 'Server is healthy' });
 });
 
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../admin/dist')));
+
+    app.get('/{*any}', (req, res) => {
+        res.sendFile(path.join(__dirname, '../admin/dist/index.html'));
+    });
+}
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
+}, (err) => {
+    if (err) {
+        console.error('Error starting server:', err);
+    }
 });
